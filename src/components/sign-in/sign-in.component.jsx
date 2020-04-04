806a71d520a389fdbae698cd060a6536c8633edf
Biswajit Behera <behera.biswa5398@gmail.com>
2020-04-04 21:50:02 +0530

    sign in handleSubmit done

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 
-import { signInWithGoogle } from "../../firebase/firebase.util";
+import { auth, signInWithGoogle } from "../../firebase/firebase.util";
 
 import "./sign-in.styles.scss";
 import FormInput from "../form-input/form-input.component";
@@ -16,8 +16,17 @@ class SignIn extends Component {
 		};
 	}
 
-	handleSubmit = (event) => {
+	handleSubmit = async (event) => {
 		event.preventDefault();
+
+		const { email, password } = this.state;
+
+		try {
+			await auth.signInWithEmailAndPassword(email, password);
+			this.setState({ email: "", password: "" });
+		} catch (error) {
+			console.log(error);
+		}
 
 		this.setState({ email: "", password: "" });
 	};
